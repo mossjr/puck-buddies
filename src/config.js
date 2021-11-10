d96247754a88d4611faa325141bb171fbee65920
MossJr <kyle.davis@hotmail.com>
2021-11-10 19:20:39 +1100

    added new jerseys

--- src/config.js
+++ src/config.js
@@ -29,23 +29,26 @@ export const marketfeesaddress = "0xb851e028EEAeb2B2eC32186AaC93E3855d5242Cc"
 //                             {admin:pbProShopHolderAddress,sku:1000290001200001,active:true,c1:'#373833',c2:'#b4b25b'},
 //                             {admin:pbProShopHolderAddress,sku:1694200002400001,active:true,c1:'#33ff99',c2:'#ff33cc'}]
 
-export const proShopItems = [{admin:pbProShopHolderAddress,sku:1694200002400001,active:true,c1:'#33ff99',c2:'#ff33cc'},
+export const proShopItems = [{admin:pbProShopHolderAddress,sku:1694200002400001,active:true,c1:'#33ff99',c2:'#ff33cc'}, 
                              {admin:pbProShopHolderAddress,sku:1100010001200001,active:true,c1:'#ae1d00',c2:'#ffc700'},
                              {admin:pbProShopHolderAddress,sku:1100020001200001,active:true,c1:'#96d8d8',c2:'#001628'},
                              {admin:pbProShopHolderAddress,sku:1100030001200001,active:true,c1:'#085d77',c2:'#0a7d04'},
                              {admin:pbProShopHolderAddress,sku:1100040001200001,active:true,c1:'#7e7e7e',c2:'#041e42'},
-                             {admin:pbProShopHolderAddress,sku:1100050001200001,active:true,c1:'#d6d289',c2:'#a0c8d7'},
-                             {admin:pbProShopHolderAddress,sku:1100060001200001,active:true,c1:'#1b1b1b',c2:'#b10000'},
-                             {admin:pbProShopHolderAddress,sku:1100070001200001,active:true,c1:'#7e893d',c2:'#ba9f5c'},
-                             {admin:pbProShopHolderAddress,sku:1100080001200001,active:true,c1:'#f4e9d8',c2:'#4c6175'},
-                             {admin:pbProShopHolderAddress,sku:1100090001200001,active:true,c1:'#ffffff',c2:'#e99a01'},
-                             {admin:pbProShopHolderAddress,sku:1100100001200001,active:true,c1:'#84b6f7',c2:'#ad38ad'},
-                             {admin:pbProShopHolderAddress,sku:1100110001200001,active:true,c1:'#121315',c2:'#4c4b49'},
-                             {admin:pbProShopHolderAddress,sku:1100120001200001,active:true,c1:'#ddc09d',c2:'#bf9674'},
-                             {admin:pbProShopHolderAddress,sku:1100130001200001,active:true,c1:'#e5b14f',c2:'#9d2c1f'},
-                             {admin:pbProShopHolderAddress,sku:1100140001200001,active:true,c1:'#04a7a6',c2:'#7c6140'},
-                             {admin:pbProShopHolderAddress,sku:1100150003000001,active:true,c1:'#ffffff',c2:'#ff0000'},
-                             {admin:pbProShopHolderAddress,sku:1100160000100001,active:false,c1:'#ffffff',c2:'#10960e'}, //Santa Suit
+                             {admin:pbProShopHolderAddress,sku:1100050001200001,active:true,c1:'#d6d289',c2:'#a0c8d7'}, //Moon
+                             {admin:pbProShopHolderAddress,sku:1100060001200001,active:true,c1:'#1b1b1b',c2:'#b10000'}, //Red Back
+                             {admin:pbProShopHolderAddress,sku:1100070001200001,active:true,c1:'#7e893d',c2:'#ba9f5c'}, //Tree hugger
+                             {admin:pbProShopHolderAddress,sku:1100080001200001,active:true,c1:'#f4e9d8',c2:'#4c6175'}, //Mr B
+                             {admin:pbProShopHolderAddress,sku:1100090001200001,active:true,c1:'#ffffff',c2:'#e99a01'}, //BNB
+                             {admin:pbProShopHolderAddress,sku:1100100001200001,active:true,c1:'#84b6f7',c2:'#ad38ad'}, //Purple Tenticle
+                             {admin:pbProShopHolderAddress,sku:1100110001200001,active:true,c1:'#121315',c2:'#4c4b49'}, //Master Chief
+                             {admin:pbProShopHolderAddress,sku:1100120001200001,active:true,c1:'#ddc09d',c2:'#bf9674'}, //Nuts
+                             {admin:pbProShopHolderAddress,sku:1100130001200001,active:true,c1:'#e5b14f',c2:'#9d2c1f'}, //CoronaVARUS
+                             {admin:pbProShopHolderAddress,sku:1100140001200001,active:true,c1:'#04a7a6',c2:'#7c6140'}, //Bob Ross
+                             {admin:pbProShopHolderAddress,sku:1100150003000001,active:true,c1:'#ffffff',c2:'#ff0000'}, //MAGA Warrior
+                             {admin:pbProShopHolderAddress,sku:1100160001200001,active:true,c1:'#ffffff',c2:'#10960e'}, //Santa Suit
+                             {admin:pbProShopHolderAddress,sku:1100170001200001,active:true,c1:'#ffffff',c2:'#10960e'}, //Is this Loss?
+                             {admin:pbProShopHolderAddress,sku:1100180001200001,active:true,c1:'#ffffff',c2:'#10960e'}, //Xmas Tree
+                             {admin:pbProShopHolderAddress,sku:1100190001200001,active:true,c1:'#ffffff',c2:'#10960e'}, //Ethereum Blue
                             ]
 
  
